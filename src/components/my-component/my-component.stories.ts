92f72f225f9e466a82b9c641fc3d1ffd9296f5c6
Schochet, Bentzion (HBO) <Bentzion.Schochet@hbo.com>
2019-02-26 11:58:33 -0500

    added jest addon config

--- src/components/my-component/my-component.stories.ts
+++ src/components/my-component/my-component.stories.ts
@@ -1,7 +1,14 @@
 import { storiesOf } from '@storybook/html';
+import results from '../../../jest-test-results.json';
+import { withTests } from '@storybook/addon-jest';
 
-storiesOf('My Component', module).add(
-  'simple',
-  () =>
-    '<my-component first="Stencil" last="\'Don\'t call me a framework\' JS"></my-component>  '
-);
+storiesOf('My Component', module)
+  .addDecorator(withTests({ results }))
+  .add(
+    'simple',
+    () =>
+      '<my-component first="Stencil" last="\'Don\'t call me a framework\' JS"></my-component>',
+    {
+      jest: ['my-component.e2e.ts']
+    }
+  );
